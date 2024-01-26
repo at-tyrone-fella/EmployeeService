a5f3227684426391eddfabe386c91f3d2d3fa045
jpoots <90725697+jpoots@users.noreply.github.com>
2024-01-26 11:59:01 +0000

    mvp

--- backend.js
+++ backend.js
@@ -94,17 +94,17 @@ app.get('/edit', (req, res) => {
 app.post('/edit' , (req,res) => {
 
 const result = req.body;
-const employeeNum = `"${parseInt(result.employeeNum)}"`
-const employeeName = `"${result.employeeName}"`
-const employeeAddress = `"${result.employeeAddress}"`
-const employeeSalary = `"${parseFloat(result.employeeSalary)}"`
-const employeeRole = `"${result.employeeRole}"`
+const employeeNum = `${parseInt(result.employeeNum)}`
+const employeeName = `${result.employeeName}`
+const employeeAddress = `${result.employeeAddress}`
+const employeeSalary = `${parseFloat(result.employeeSalary)}`
+const employeeRole = `${result.employeeRole}`
 
 if(employeeName == "" || employeeAddress == "" || employeeSalary == "" || employeeRole == "")
 {
   res.redirect("/viewEmployee")
 }else{
-const insertQuery = `UPDATE Employee SET Name = ${employeeName}, Address = ${employeeAddress}, Role = ${employeeRole}, Employee_Number = ${employeeNum}, Salary = ${employeeSalary} WHERE Employee.Employee_Number = ${employeeNum}`;
+const insertQuery = `UPDATE Employee SET Name = "${employeeName}", Address = "${employeeAddress}", Role = "${employeeRole}", Employee_Number = ${employeeNum}, Salary = ${employeeSalary} WHERE Employee.Employee_Number = ${employeeNum}`;
 
 connection.query(insertQuery , (error, result) => {
   console.log(error)
