d6352efd46f68f86f2cff110b672330348163178
jpoots <90725697+jpoots@users.noreply.github.com>
2024-01-26 11:07:56 +0000

    work in progress

--- Kainos_Employee_System.sql
+++ Kainos_Employee_System.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 25, 2024 at 11:32 AM
+-- Generation Time: Jan 26, 2024 at 10:05 AM
 -- Server version: 5.7.39
 -- PHP Version: 7.4.33
 
@@ -44,6 +44,26 @@ INSERT INTO `Employee` (`Employee_Id`, `Name`, `Address`, `Salary`, `Role`, `Emp
 (1, 'Steve Jobs', '123 Apple Village', 1000000, 'CEO', 1),
 (2, 'Elon Musk', '1 X Town', 900000, 'Chief of Stuff', 2);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `LoginDetails`
+--
+
+CREATE TABLE `LoginDetails` (
+  `Login_ID` int(11) NOT NULL,
+  `Email` varchar(320) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `LoginDetails`
+--
+
+INSERT INTO `LoginDetails` (`Login_ID`, `Email`, `Password`) VALUES
+(1, 'example@gmail.com', 'password'),
+(2, 'example2@gmail.com', 'password');
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +75,12 @@ ALTER TABLE `Employee`
   ADD PRIMARY KEY (`Employee_Id`);
 
 --
+-- Indexes for table `LoginDetails`
+--
+ALTER TABLE `LoginDetails`
+  ADD PRIMARY KEY (`Login_ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +89,12 @@ ALTER TABLE `Employee`
 --
 ALTER TABLE `Employee`
   MODIFY `Employee_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `LoginDetails`
+--
+ALTER TABLE `LoginDetails`
+  MODIFY `Login_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- backend.js
+++ backend.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const session = require('express-session');
 const http = require("http")
 const path = require("path")
 const mysql = require("mysql2")
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const { render } = require('ejs');
 const LocalStrategy = require('passport-local').Strategy;
 
 const app = express()
@@ -15,7 +17,7 @@ app.use('/public', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// connect to d
+// connect to db
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -83,3 +85,37 @@ app.post('/add', (req, res) => {
   })
   
 });
+
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(authUser))
+
+function authUser (user, password, done) {
+  //Search the user, password in the DB to authenticate the user
+  //Let's assume that a search within your DB returned the username and password match for "Kyle".
+     let authenticated_user = { id: 123, name: "Kyle"}
+     return done (null, authenticated_user )
+  }
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+
+// Deserialize user from the session
+passport.deserializeUser((id, done) => {
+  const user = users.find(u => u.id === id);
+  done(null, user);
+});
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login',
+  passport.authenticate('local',
+   { successRedirect: '/', failureRedirect: '/login' })
+);
